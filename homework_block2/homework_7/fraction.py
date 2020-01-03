a6020a0fadef1d2857c8363a604593ce2af030e0
Your Name <sisoko1110ok>
2020-01-03 20:33:29 +0300

    Add fraction.py

---
+++ homework_block2/homework_7/fraction.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+
+
+import math
+
+
+class Fraction:
+    """Класс поддерживает все арифметические операции над дробями"""
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        return str(self.numerator) + "/" + str(self.denominator)
+
+    def __add__(self, other):
+        numerator_new = self.numerator * other.denominator + self.denominator * other.numerator
+        denominator_new = self.denominator * other.denominator
+        gcd = math.gcd(numerator_new, denominator_new)
+        return Fraction(numerator_new // gcd, denominator_new // gcd)
+
+    def __sub__(self, other):
+        numerator_new = self.numerator * other.denominator - self.denominator * other.numerator
+        denominator_new = self.denominator * other.denominator
+        gcd = math.gcd(numerator_new, denominator_new)
+        return Fraction(numerator_new // gcd, denominator_new // gcd)
+
+    def __mul__(self, other):
+        numerator_new = self.numerator * other.numerator
+        denominator_new = self.denominator * other.denominator
+        gcd = math.gcd(numerator_new, denominator_new)
+        return Fraction(numerator_new // gcd, denominator_new // gcd)
+
+    def __truediv__(self, other):
+        numerator_new = self.numerator * other.denominator
+        denominator_new = self.denominator * other.numerator
+        gcd = math.gcd(numerator_new, denominator_new)
+        return Fraction(numerator_new // gcd, denominator_new // gcd)
+
+
+if __name__ == "__main__":
+    my_fraction1 = Fraction(1, 2)
+    my_fraction2 = Fraction(3, 4)
+    my_fraction3 = Fraction(1, 9)
+    my_fraction4 = Fraction(3, 5)
+
+    print(my_fraction1+my_fraction2+my_fraction3*my_fraction4)
